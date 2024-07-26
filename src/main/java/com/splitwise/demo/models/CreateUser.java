d67dcdde583fc7e564dc485ef33d9de5423fe0e7
vaibhavkhandelwal <vaibhavkhandelwal22@gmail.com>
2024-07-26 12:58:31 +0530

    Update to test merge squash

--- src/main/java/com/splitwise/demo/models/CreateUser.java
+++ src/main/java/com/splitwise/demo/models/CreateUser.java
@@ -4,17 +4,25 @@ public class CreateUser {
     String userName;
 
     public CreateUser(String userName) {
+
         this.userName = userName;
     }
 
     public CreateUser() {
+
+
     }
 
     public String getUserName() {
+
+
+
         return userName;
     }
 
     public void setUserName(String userName) {
+
+
         this.userName = userName;
     }
 }
